026a2c2a067be08042f888ff8b5dac71a1f4d1bf
Jeffrey Huang <jeffvh@outlook.com>
2019-09-28 14:52:41 +0100

    add dotenv package. Use api key and storage key via env variables

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
-import { apiKey } from "./constants";
-import { apiResponse } from "./tennis-tournaments";
-
-const atpLevels = ["atp_250", "atp_500", "atp_1000", "grand_slam"];
+import { API_KEY } from "./constants";
+import { apiResponse } from "./tennis-tournaments"; // mock data
 
 // Functions to be used in action creators where error catching will occur
 export const getTournaments = async () => {
   const response = await axios.get(
-    `https://api.sportradar.com/tennis-t2/en/tournaments.json?api_key=${apiKey}`
+    `https://api.sportradar.com/tennis-t2/en/tournaments.json?api_key=${API_KEY}`
   );
   // const response = apiResponse;
 
